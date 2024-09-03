64b8be9fc6f7e27545b290b828908b859c5fc280
Shiwangi Maurya <111489964+Shiwangii@users.noreply.github.com>
2024-09-03 23:08:53 +0530

    Add convert-1d-array-into-2d-array.cpp

---
+++ convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int res[][]=new int[m][n];
+        int p=0;
+        if((m*n)!=original.length)return new int[0][0];
+        for(int i=0; i<original.length; i++){
+            res[i/n][i%n]=original[i];
+            
+        }
+        return res;
+        
+    }
+}
