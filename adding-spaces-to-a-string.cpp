27b8a7673c3dc6aa987fca94de4e2f0f7ac4cceb
Shiwangi Maurya <111489964+Shiwangii@users.noreply.github.com>
2024-12-03 22:57:37 +0530

    Add adding-spaces-to-a-string.cpp

---
+++ adding-spaces-to-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+
+    public String addSpaces(String s, int[] spaces) {
+        StringBuilder result = new StringBuilder();
+        result.ensureCapacity(s.length() + spaces.length);
+
+        int spaceIndex = 0;
+        for (int stringIndex = 0; stringIndex < s.length(); ++stringIndex) {
+            if (
+                spaceIndex < spaces.length && stringIndex == spaces[spaceIndex]
+            ) {
+                result.append(' ');
+                ++spaceIndex;
+            }
+            result.append(s.charAt(stringIndex));
+        }
+
+        return result.toString();
+    }
+}
