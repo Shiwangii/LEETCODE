49fa81b6ea88254c9dd4a042bbb5aa891d029645
Shiwangi Maurya <111489964+Shiwangii@users.noreply.github.com>
2024-12-20 00:36:16 +0530

    Add max-chunks-to-make-sorted.cpp

---
+++ max-chunks-to-make-sorted.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n=arr.length;
+        int chunks=0, maxElement=0;
+        for(int i=0; i<n; i++){
+            maxElement=Math.max(maxElement, arr[i]);
+            if(maxElement==i){
+                chunks++;
+            }
+        }
+        return chunks;
+    }
+}
